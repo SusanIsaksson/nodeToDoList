1f709fa695f09656ba60933f505bc1b985c5d9a6
susanisaksson <susan.isaksson@medieinstitutet.se>
2021-08-26 15:30:58 +0200

    Update

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const fs = require('fs');
+const fs = require('fs');  // for saving in json-fil
 const express = require('express')
 const server = express()
 const port = 3000
-
-//let colors = []
 
 server.use(express.json())
 
 server.get('/api', (req, res) => {
     let raw = fs.readFileSync("todos.json")
     let todoItem = JSON.parse(raw)
+    //res.json(req.body)
     res.json(todoItem)  //ersatt .send med .json pga undefined
+    res.render('index', {todoItem: todoItem, complete: complete});
+    
 })
 
 server.post('/api', (req, res) => {
